86253284946768728490fd107a036d7b7dc2ca78
chinosama <85371007+chinosama@users.noreply.github.com>
2021-06-16 10:33:35 -0300

    agrega html

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+const { ENGINE_METHOD_PKEY_ASN1_METHS } = require("constants");
 const express = require("express");
 const app = express();
 const port = 3030;
+const path = require("path");
+app.use(express.static('public'));
 
-app.get("/",(req,res) => res.send("Bienvenidos a mi sitio web"))
+
+app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html")));
+
+app.get("/contact",(req,res) => res.sendFile(path.join(__dirname,"views","contact.html")));
+
+app.get("/about",(req,res) => res.sendFile(path.join(__dirname,"views","about.html")));
 
 app.listen(3030,()=> console.log("servidor corriendo en http://localhost:"+ port))
